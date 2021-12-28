7c078e04d4dd1c34b2c54a49c348e86616934f69
Pratik Singh <73792866+pratiks3208@users.noreply.github.com>
2021-12-28 12:50:07 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component!!!
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam illum ut assumenda quos natus libero aliquam odit voluptates impedit veniam, placeat, expedita nisi earum tenetur, rem voluptatum ratione. Eius nobis aspernatur fugit voluptates magni.</p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Footer = () => {
+
+    // let footerStyle = {
+    //     position: "relative",
+    //     top: "10vh",
+    //     width: "100%",
+    //     border: "5px solid green"
+
+    // }
+    return (
+        <footer className="bg-dark text-light py-2">
+            <p className="text-center">
+                Copyright &copy; MyTodosList.com 
+                All rights reserved
+            </p>
+        </footer>
+    )
+}
